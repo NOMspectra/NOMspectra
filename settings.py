15e86144fbc63b421f365d9b73f3d60963acd299
Gleb Rukhovich <rukhovich@gmail.com>
2020-05-04 03:47:39 +0300

    Emplemented using elements like "13C", "2H"

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ from pathlib import Path
 PWD = os.path.dirname(os.path.realpath(__file__ ))
 
 MONOISOTOPIC_MASSES_PATH = Path(PWD) / "masses" / "monoisotopic_masses.csv"
+ISOTOPIC_MASSES_PATH = Path(PWD) / "masses" / "isotopic_masses.csv"
 ISOTOPE_ABUNDANCE_PATH = Path(PWD) / "masses" / "isotope_abundance.csv"
 AVERAGE_MASSES_PATH = Path(PWD) / "masses" / "average_masses.csv"
 DATA_FOLDER = Path(PWD) / "data"
