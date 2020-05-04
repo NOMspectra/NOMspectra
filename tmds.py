bb1c2b6c5e23d5436bf2f49499e8e7d98a10da1d
Gleb Rukhovich <rukhovich@gmail.com>
2020-05-04 05:18:39 +0300

    Implemented default tmds by numpy cartesian multiplication (should be fase)

--- tmds.py
+++ tmds.py
@@ -1,4 +1,12 @@
+import numpy as np
+import pandas as pd
+from typing import Sequence
+
 from mass import MassSpectrum
+
+
+def _make_smooth(X: np.ndarray) -> np.ndarray:
+    return X
 
 
 def _calculate_tmds_by_default(mass_spectrum: MassSpectrum) -> MassSpectrum:
@@ -7,7 +15,25 @@ def _calculate_tmds_by_default(mass_spectrum: MassSpectrum) -> MassSpectrum:
     :param mass_spectrum:
     :return:
     """
-    raise NotImplementedError
+
+    mass_spectrum = mass_spectrum.copy()
+    mass_spectrum.table["I"] = mass_spectrum.table["I"] / mass_spectrum.table["I"].sum()
+
+    # this 2 lines makes all calculations, pure magic :)
+    x = mass_spectrum[["mass", "I"]].values
+    table = pd.DataFrame(
+        np.hstack([np.tile(x.T, len(x)).T, np.repeat(x, len(x), axis=0)]),
+        columns=["mass_1", "probability_1", "mass_2", "probability_2"]
+    )
+    table["mass"] = table["mass_1"] - table["mass_2"]
+    table["probability"] = table["probability_1"] * table["probability_2"]
+
+    # filtering, only masses > 0
+    table = table[table.mass > 0]
+
+    # FIXME here we need to smooth
+    table["I"] = table["probability"]
+    return MassSpectrum(table[["mass", "I"]])
 
 
 def _calculate_tmds_by_stochastic(mass_spectrum: MassSpectrum) -> MassSpectrum:
