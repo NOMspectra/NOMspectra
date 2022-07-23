34440ec30b0f77bc42b23855756b5d4871d33574
Alexander Volikov <ab.volikov@gmail.com>
2022-07-23 15:39:55 +0300

    prepare for pypi

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     description='A small lib for treatment high-resolution mass spectrum',
     long_description=open('README.md').read(),
     author='Volikov Alexander, Rukhovich Gleb',
+    url = 'https://github.com/nhsmasslib/nhsmasslib',
+    download_url = 'https://github.com/nhsmasslib/nhsmasslib/archive/refs/tags/v0.1.3.tar.gz',
     install_requires=[
         'matplotlib>=3.1.2',
         'numpy>=1.18.1',
         'pandas>=0.25.3',
-        'scikit-learn>=0.22.1',
         'scipy>=1.4.1',
         'seaborn>=0.10.0',
         'tqdm>=4.43.0',
